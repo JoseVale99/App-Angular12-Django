9d76cd06984e25f31a63c381a72fb08e90d8c212
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-01 23:38:12 -0500

    add index view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DepartamentComponent } from './departament/departament.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DepartamentComponent
   ],
   imports: [
     BrowserModule,
